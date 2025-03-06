4a9c752b38651ed6a583d8ef3a10ff98c50aade5
yashbim <ybimsara03@gmail.com>
2025-03-06 13:49:35 +0530

    Added error handling for empty fields in create account page

--- app/src/main/java/com/example/city_cycle_app/CreateAccount.kt
+++ app/src/main/java/com/example/city_cycle_app/CreateAccount.kt
@@ -11,16 +11,25 @@ import androidx.appcompat.app.AppCompatActivity
 class CreateAccount : AppCompatActivity() {
 
     private var pwStatus = "rejected"
+    private var emptyFields = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_acc_page)
-        
+
+
         val proceedToLogin_button: Button = findViewById(R.id.proceed_to_login_button)
         proceedToLogin_button.setOnClickListener {
 
+            emptyFieldsCheck()
             passwordSimilarityCheck();
 
-            if (pwStatus == "approved"){
+            if (emptyFields == true){
+                Toast.makeText(
+                    applicationContext,
+                    "Fill out all fields",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else if (pwStatus == "approved"){
                 Toast.makeText(
                     applicationContext,
                     "Log in with your new credentials",
@@ -29,7 +38,7 @@ class CreateAccount : AppCompatActivity() {
 
                 val intent_proceedToLogin_button = Intent(this, LoginPage::class.java)
                 startActivity(intent_proceedToLogin_button)
-            } else {
+            } else if(pwStatus == "rejected"){
                 Toast.makeText(
                     applicationContext,
                     "Passwords do not match",
@@ -52,5 +61,35 @@ class CreateAccount : AppCompatActivity() {
 
         pwStatus = if (password1 == password2) "approved" else "rejected"
 
+    }
+
+    private fun emptyFieldsCheck(){
+        val inputEmail = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val inputEmail_value = inputEmail.text.toString().trim()
+        val inputPW = findViewById<EditText>(R.id.editTextTextPassword)
+        val inputPW_value = inputPW.text.toString().trim()
+        val inputConfirmPW = findViewById<EditText>(R.id.editTextreneterpw)
+        val inputConfirmPW_value = inputConfirmPW.text.toString().trim()
+
+        if (inputEmail_value.isEmpty()){
+            emptyFields = true
+        } else if (inputPW_value.isEmpty()){
+            emptyFields = true
+        } else if (inputConfirmPW_value.isEmpty()){
+            emptyFields = true
+        } else {
+            emptyFields = false
+        }
+    }
+
+    private fun proceedToLogin(){
+        Toast.makeText(
+            applicationContext,
+            "Log in with your new credentials",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        val intent_proceedToLogin_button = Intent(this, LoginPage::class.java)
+        startActivity(intent_proceedToLogin_button)
     }
 }
