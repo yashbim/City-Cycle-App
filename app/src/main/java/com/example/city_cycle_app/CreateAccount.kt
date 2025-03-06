7d24b5939ea50b47212caf20f6d09e0a881f8b16
yashbim <ybimsara03@gmail.com>
2025-03-06 10:29:19 +0530

    Added logic for matching password

--- app/src/main/java/com/example/city_cycle_app/CreateAccount.kt
+++ app/src/main/java/com/example/city_cycle_app/CreateAccount.kt
@@ -3,25 +3,60 @@ package com.example.city_cycle_app
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class CreateAccount : AppCompatActivity() {
 
+    private var pwStatus = "rejected"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_acc_page)
-
+        
         val proceedToLogin_button: Button = findViewById(R.id.proceed_to_login_button)
-        proceedToLogin_button.setOnClickListener{
+        proceedToLogin_button.setOnClickListener {
 
-            Toast.makeText(applicationContext,"Log in with your new credentials",Toast.LENGTH_SHORT).show()
+            passwordSimilarityCheck();
 
-            val intent_proceedToLogin_button = Intent(this, LoginPage::class.java)
-            startActivity(intent_proceedToLogin_button)
+            if (pwStatus == "approved"){
+                Toast.makeText(
+                    applicationContext,
+                    "Log in with your new credentials",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                val intent_proceedToLogin_button = Intent(this, LoginPage::class.java)
+                startActivity(intent_proceedToLogin_button)
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Passwords do not match",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
         }
 
     }
 
+    private fun passwordSimilarityCheck() {
+        val pw1: EditText = findViewById(R.id.editTextTextPassword)
+        val password1 = pw1.text.toString()
+        print(password1)
+
+        val pw2: EditText = findViewById(R.id.editTextreneterpw)
+        val password2 = pw2.text.toString()
+        print(password2)
+
+//        if (password1 == password2){
+//            val pwStatus = "approved"
+//        } else {
+//            val pwStatus = "rejected"
+//        }
+
+        pwStatus = if (password1 == password2) "approved" else "rejected"
+
+    }
 }
