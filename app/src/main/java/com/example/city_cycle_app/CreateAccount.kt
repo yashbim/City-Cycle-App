ab803150b76f9ecf5ceab87ad8939285700af7e1
yashbim <ybimsara03@gmail.com>
2025-03-06 09:20:09 +0530

    Implemented button from create account to login

--- app/src/main/java/com/example/city_cycle_app/CreateAccount.kt
+++ app/src/main/java/com/example/city_cycle_app/CreateAccount.kt
@@ -1,6 +1,8 @@
 package com.example.city_cycle_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class CreateAccount : AppCompatActivity() {
@@ -8,6 +10,12 @@ class CreateAccount : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_acc_page)
+
+        val proceedToLogin_button: Button = findViewById(R.id.proceed_to_login_button)
+        proceedToLogin_button.setOnClickListener{
+            val intent_proceedToLogin_button = Intent(this, LoginPage::class.java)
+            startActivity(intent_proceedToLogin_button)
+        }
 
     }
 
