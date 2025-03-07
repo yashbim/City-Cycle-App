2cb1e4ccaf5de538576b93624e0eed102bbab53c
yashbim <ybimsara03@gmail.com>
2025-03-07 14:22:56 +0530

    Added landing page to manifest

---
+++ app/src/main/java/com/example/city_cycle_app/LandingPage.kt
@@ -0,0 +1,13 @@
+package com.example.city_cycle_app
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LandingPage : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_page)
+
+    }
+}
--- app/src/main/java/com/example/city_cycle_app/LoginPage.kt
+++ app/src/main/java/com/example/city_cycle_app/LoginPage.kt
@@ -1,12 +1,26 @@
 package com.example.city_cycle_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlin.math.log
 
 class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        val login_button: Button = findViewById(R.id.proceed_to_login_button)
+        login_button.setOnClickListener{
+            val intent_login = Intent(this, LandingPage::class.java)
+            startActivity(intent_login)
+        }
+
+        //login page now working
     }
+
+
+
 
 }
