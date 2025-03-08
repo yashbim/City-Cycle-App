22c80888889bea2261cba5c4ed98acff433ff57e
yashbim <ybimsara03@gmail.com>
2025-03-08 15:09:59 +0530

    Changed button and bg colors

--- app/src/main/java/com/example/city_cycle_app/LandingPage.kt
+++ app/src/main/java/com/example/city_cycle_app/LandingPage.kt
@@ -1,13 +1,18 @@
 package com.example.city_cycle_app
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LandingPage : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_page)
+
+//        val find_bikes_button : Button = findViewById(R.id.book_bikes_button)
+//        find_bikes_button.setOnClickListener{
+//
+//        }
 
     }
 }
