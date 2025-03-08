2f630408fc9ea41371702b89c8e660b7a7e4574c
yashbim <ybimsara03@gmail.com>
2025-03-08 15:17:31 +0530

    Added onclick function to book bikes leading to book bikes page

--- app/src/main/java/com/example/city_cycle_app/LandingPage.kt
+++ app/src/main/java/com/example/city_cycle_app/LandingPage.kt
@@ -1,5 +1,6 @@
 package com.example.city_cycle_app
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -9,10 +10,11 @@ class LandingPage : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_page)
 
-//        val find_bikes_button : Button = findViewById(R.id.book_bikes_button)
-//        find_bikes_button.setOnClickListener{
-//
-//        }
+        val find_bikes_button : Button = findViewById(R.id.book_bikes_button)
+        find_bikes_button.setOnClickListener{
+            val intent_to_book_bikes = Intent(this, BookBikes::class.java)
+            startActivity(intent_to_book_bikes)
+        }
 
     }
 }
